4dc321c975290131cbda7e40925a5dfe52846901
Ognjen01 <lazicognjen2001@gmail.com>
2021-08-11 20:17:08 +0200

    Small changes

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -17,73 +17,74 @@ class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: [],
-          iconTheme: IconThemeData(
-            color: Color(0xff080947),
+          appBar: AppBar(
+            actions: [],
+            iconTheme: IconThemeData(
+              color: Color(0xff080947),
+            ),
+            backgroundColor: Color(0xff9dcbbc),
+            title: Text(
+              map.name,
+              style: TextStyle(
+                  color: Color(0xff080947), fontWeight: FontWeight.bold),
+            ),
           ),
-          backgroundColor: Color(0xff9dcbbc),
-          title: Text(
-            map.name,
-            style: TextStyle(
-                color: Color(0xff080947), fontWeight: FontWeight.bold),
-          ),
-        ),
-        body: Center(
-            child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: PhotoView(
-            imageProvider: NetworkImage('${map.mapUrl}'),
-            backgroundDecoration: BoxDecoration(color: Color(0xffffffff)),
-          ),
-        )),
-        floatingActionButton: Stack(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 31),
-              child: Align(
-                alignment: Alignment.bottomLeft,
+          body: Center(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: PhotoView(
+              imageProvider: NetworkImage('${map.mapUrl}'),
+              backgroundDecoration: BoxDecoration(color: Color(0xffffffff)),
+            ),
+          )),
+          floatingActionButton: Stack(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 31),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: FloatingActionButton(
+                      heroTag: "btn1",
+                      child: Text(
+                        "gpx",
+                        style: TextStyle(color: Color(0xff080947)),
+                      ),
+                      backgroundColor: Color(0xff9dcbbc),
+                      onPressed: () async => {
+                            print("Početak preuzimanja! "),
+                            //await downloadFile(map.gpxUrl, map.name)
+                            await downloadWithAnotherPackage(map, context)
+                            // Preuzimanje gpx traga
+                          }),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
                 child: FloatingActionButton(
-                    heroTag: "btn1",
-                    child: Text(
-                      "gpx",
-                      style: TextStyle(color: Color(0xff080947)),
+                    heroTag: "btn2",
+                    child: Icon(
+                      Icons.download_sharp,
+                      color: Color(0xff080947),
                     ),
                     backgroundColor: Color(0xff9dcbbc),
                     onPressed: () async => {
-                          print("Početak preuzimanja! "),
-                          //await downloadFile(map.gpxUrl, map.name)
-                          await downloadWithAnotherPackage(map, context)
-                          // Preuzimanje gpx traga
+                          await ImageDownloader.downloadImage(map.mapUrl)
+                              .catchError((e) {
+                            showDialog(
+                                context: context,
+                                builder: (_) => AlertDialog(
+                                      title: Text("Greška"),
+                                      content: Text(
+                                          "Došlo je do greške, molimo pokušajte ponovo!"),
+                                    ));
+                          }),
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text("Uspješno preuzeta fotografija")))
                         }),
               ),
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: FloatingActionButton(
-                  heroTag: "btn2",
-                  child: Icon(
-                    Icons.download_sharp,
-                    color: Color(0xff080947),
-                  ),
-                  backgroundColor: Color(0xff9dcbbc),
-                  onPressed: () async => {
-                        await ImageDownloader.downloadImage(map.mapUrl)
-                            .catchError((e) {
-                          showDialog(
-                              context: context,
-                              builder: (_) => AlertDialog(
-                                    title: Text("Greška"),
-                                    content: Text(
-                                        "Došlo je do greške, molimo pokušajte ponovo!"),
-                                  ));
-                        }),
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text("Uspješno preuzeta fotografija")))
-                      }),
-            ),
-          ],
-        ));
+            ],
+          ),
+    );
   }
 
   void downloadWithAnotherPackage(Map mapa, BuildContext context) async {
